52d9e1809620cb46553760b4c10df15190b1fa38
Junho jeon <junhjeon@Junhoui-MacBookAir.local>
2025-07-26 20:05:34 +0900

    init

---
+++ src/test/java/com/example/lucky_backend/LuckyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lucky_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
